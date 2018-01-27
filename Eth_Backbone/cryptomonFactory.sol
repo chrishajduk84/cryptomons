0ee93231d26ddca32176c9ef08c0bf6e54b80379
Chris Hajduk <chrishajduk84@hotmail.com>
2018-01-27 13:44:21 -0500

    Added ability to call function based on wallet contract address.

--- Eth_Backbone/cryptomonFactory.sol
+++ Eth_Backbone/cryptomonFactory.sol
@@ -13,21 +13,31 @@ contract cryptomonFactory{
     }
 
  mapping(int => uint) monsType;
+ mapping(bytes32 => address) availableMons; //Is this Mining?
 
- function cryptomonFactory(address _createdby) public{
+ function cryptomonFactory(address _createdby) internal{
      owner = _createdby;
  }
  
- void getNextMons(int locX, int locY, address walletAddress){
-    walletAddress.call(addCryptoMon)    
- 
+ function getNextMons(int locX, int locY, address walletAddress, bytes32 hash) external{
+    bytes32 compHash = sha256(locX,locY,walletAddress);
+    require(hash == compHash); //We can verify that walletAddres is at location X/Y
+    //But how do we prevent people from spamming this function all the time?
+    //Some type of time dependant hashing needs to be done to determine when things happen and only allow this function to run then
+    
+    //Assign token based on hash, use a mapping - TODO - Pull from queue/reserved cryptos
+    require(availableMons[hash] != 0);
+    if (!walletAddress.call(bytes4(keccak256("addCryptomon(address)")), availableMons[hash])){
+        revert();
+    }
+    
  }
  
  function isContract(address addr) private view returns (bool) {
   uint size;
   assembly { size := extcodesize(addr) }
   return size > 0;
-}
+ }
     
 
 }
